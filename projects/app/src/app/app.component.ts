4d768094f545a1653454f33bffd156dafe54922f
rbilmakovskyi <ruslan.bilmakovskyi@ekreative.com>
2020-07-01 11:32:47 +0300

    added favourite colors

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -10,9 +10,9 @@ import { ColorPickerService, Cmyk } from 'ngx-color-picker';
 })
 export class AppComponent {
   public toggle: boolean = false;
-
+  
   public rgbaText: string = 'rgba(165, 26, 214, 0.2)';
-
+  
   public arrayColors: any = {
     color1: '#2883e9',
     color2: '#e920e9',
@@ -20,9 +20,9 @@ export class AppComponent {
     color4: 'rgb(236,64,64)',
     color5: 'rgba(45,208,45,1)'
   };
-
+  
   public selectedColor: string = 'color1';
-
+  
   public color1: string = '#2889e9';
   public color2: string = '#e920e9';
   public color3: string = '#fff500';
@@ -41,40 +41,48 @@ export class AppComponent {
   public color16: string = '#a51ad633';
   public color17: string = '#666666';
   public color18: string = '#ff0000';
-
+  
   public cmykValue: string = '';
-
+  
   public cmykColor: Cmyk = new Cmyk(0, 0, 0, 0);
-
+  
   constructor(public vcRef: ViewContainerRef, private cpService: ColorPickerService) {}
-
+  
+  public addFavColor(event: string): void {
+    console.log(event);
+  }
+  
+  public removeFavColor(event: string): void {
+    console.log(event);
+  }
+  
   public onEventLog(event: string, data: any): void {
     console.log(event, data);
   }
-
+  
   public onChangeColor(color: string): void {
     console.log('Color changed:', color);
   }
-
+  
   public onChangeColorCmyk(color: string): Cmyk {
     const hsva = this.cpService.stringToHsva(color);
-
+    
     if (hsva) {
       const rgba = this.cpService.hsvaToRgba(hsva);
-
+      
       return this.cpService.rgbaToCmyk(rgba);
     }
-
+    
     return new Cmyk(0, 0, 0, 0);
   }
-
+  
   public onChangeColorHex8(color: string): string {
     const hsva = this.cpService.stringToHsva(color, true);
-
+    
     if (hsva) {
       return this.cpService.outputFormat(hsva, 'rgba', null);
     }
-
+    
     return '';
   }
 }
